3a82acfff6ab7e8e2fa6569993bdf3eefa7716d5
kevin frile <kafraile@misena.edu.co>
2025-06-26 21:47:32 -0500

    precarga y usa las imágenes directamente desde memoria (no del disco)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { NgClass } from '@angular/common';
-import { Component, AfterViewInit, ViewChild, ElementRef, QueryList, ViewChildren, Input } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, HostListener, QueryList, ViewChildren, Input } from '@angular/core';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
@@ -13,105 +13,152 @@ gsap.registerPlugin(ScrollTrigger);
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements AfterViewInit {
+
   @Input() modoOscuro = false;
 
+
   conocimientosArray = [
-    { nombre: "HTML", imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/html.svg", descripcion: "Lo utilizo para estructurar el contenido de páginas web." },
-    { nombre: "CSS", imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/css.svg", descripcion: "Lo uso para darle estilo y diseño visual a las interfaces web." },
-    { nombre: "JavaScript", imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/js.svg", descripcion: "Lenguaje con el que añado interactividad y lógica a las aplicaciones web." },
-    { nombre: "MySQL", imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/mysql.svg", descripcion: "Lo utilizo para gestionar bases de datos relacionales y hacer consultas." },
-    { nombre: "Angular", imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/angular.svg", descripcion: "Framework que utilizo para desarrollar aplicaciones web SPA robustas." },
-    { nombre: "Ionic", imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/ionic.svg", descripcion: "Lo uso para crear aplicaciones móviles híbridas con tecnologías web." },
-    { nombre: "Bootstrap", imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/bootstrap.svg", descripcion: "Framework que utilizo para construir interfaces web responsivas rápidamente." },
-    { nombre: "", imagen: "", descripcion: "" },
-    { nombre: "Figma", imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/figma.svg", descripcion: "Herramienta que utilizo para diseñar prototipos UI/UX de interfaces." },
-    { nombre: "Spline", imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/spline.svg", descripcion: "Lo uso para crear y animar elementos 3D interactivos para la web." },
-    { nombre: "GSAP", imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/gsap.svg", descripcion: "Librería que utilizo para hacer animaciones fluidas y personalizadas en sitios web." },
-    { nombre: "Python", imagen: "https://img.icons8.com/ios7/600/FFFFFF/python.png", descripcion: "Lenguaje que uso para lógica backend, automatización y procesamiento de datos." },
-    { nombre: "Flask", imagen: "https://assets.streamlinehq.com/image/private/w_300,h_300,ar_1/f_auto/v1/icons/3/flask-oabo4nn8vibtrqvucoolel.png/flask-bcpzvlxe9vryvu8v3etb.png?_a=DATAdtAAZAA0", descripcion: "Microframework de Python que utilizo para crear APIs y servicios web." }
+    {
+      nombre: "HTML",
+      imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/html.svg",
+      descripcion: "Lo utilizo para estructurar el contenido de páginas web."
+    },
+    {
+      nombre: "CSS",
+      imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/css.svg",
+      descripcion: "Lo uso para darle estilo y diseño visual a las interfaces web."
+    },
+    {
+      nombre: "JavaScript",
+      imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/js.svg",
+      descripcion: "Lenguaje con el que añado interactividad y lógica a las aplicaciones web."
+    },
+    {
+      nombre: "MySQL",
+      imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/mysql.svg",
+      descripcion: "Lo utilizo para gestionar bases de datos relacionales y hacer consultas."
+    },
+    {
+      nombre: "Angular",
+      imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/angular.svg",
+      descripcion: "Framework que utilizo para desarrollar aplicaciones web SPA robustas."
+    },
+    {
+      nombre: "Ionic",
+      imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/ionic.svg",
+      descripcion: "Lo uso para crear aplicaciones móviles híbridas con tecnologías web."
+    },
+    {
+      nombre: "Bootstrap",
+      imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/bootstrap.svg",
+      descripcion: "Framework que utilizo para construir interfaces web responsivas rápidamente."
+    },
+    {
+      nombre: "",
+      imagen: "",
+      descripcion: ""
+    },
+    {
+      nombre: "Figma",
+      imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/figma.svg",
+      descripcion: "Herramienta que utilizo para diseñar prototipos UI/UX de interfaces."
+    },
+    {
+      nombre: "Spline",
+      imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/spline.svg",
+      descripcion: "Lo uso para crear y animar elementos 3D interactivos para la web."
+    },
+    {
+      nombre: "GSAP",
+      imagen: "https://portafolio-puk64inbr-kevinfrile.vercel.app/assets/gsap.svg",
+      descripcion: "Librería que utilizo para hacer animaciones fluidas y personalizadas en sitios web."
+    },
+    {
+      nombre: "Python",
+      imagen: "https://img.icons8.com/ios7/600/FFFFFF/python.png",
+      descripcion: "Lenguaje que uso para lógica backend, automatización y procesamiento de datos."
+    },
+    {
+      nombre: "Flask",
+      imagen: "https://assets.streamlinehq.com/image/private/w_300,h_300,ar_1/f_auto/v1/icons/3/flask-oabo4nn8vibtrqvucoolel.png/flask-bcpzvlxe9vryvu8v3etb.png?_a=DATAdtAAZAA0",
+      descripcion: "Microframework de Python que utilizo para crear APIs y servicios web."
+    }
   ];
 
+
+  // Estados para recargar videos
   zorroVideoSaludoSaludo = true;
   zorroVideoConocimientos = true;
 
   segundos = 6;
   videoDuration = 8;
 
+
+
   @ViewChild('zorroVideoSaludoSaludo') zorroVideoSaludo!: ElementRef<HTMLVideoElement>;
   @ViewChild('zorroVideoConocimientos', { static: true }) zorroVideoConocimiento!: ElementRef<HTMLVideoElement>;
-  @ViewChild('scrollImage', { static: true }) scrollImageRef!: ElementRef<HTMLImageElement>;
-  @ViewChild('scrollContainer', { static: true }) containerRef!: ElementRef<HTMLDivElement>;
-  @ViewChildren('card') cards!: QueryList<ElementRef>;
 
-  totalFrames = 232;
-  preloadedImages: HTMLImageElement[] = [];
+
 
   ngAfterViewInit(): void {
-    this.preloadFrames().then(() => {
-      this.inicializarVideoScroll();
-    });
-
+    this.preloadFrames();
     this.setupVideoObserver();
-    this.animacionCard();
+    this.inicializarVideoScroll()
+    this.animacionCard()
   }
+  preloadedImages: HTMLImageElement[] = [];
 
-  async preloadFrames(): Promise<void> {
-    this.preloadedImages = [];
+  preloadFrames(): void {
 
-    this.totalFrames = this.modoOscuro ? 240 : 232;
-
-    const promises = [];
+    this.preloadedImages = []; // limpiar por si acaso
 
     for (let i = 0; i < this.totalFrames; i++) {
       const padded = String(i).padStart(3, '0');
-      const path = this.modoOscuro
-        ? `assets/scroll-video-oscuro/frame_${padded}.jpg`
-        : `assets/scroll-video/frame_${padded}.jpg`;
-
       const img = new Image();
-      img.src = path;
-
-      const promise = new Promise<void>((resolve) => {
-        img.onload = () => resolve();
-        img.onerror = () => resolve(); // evita que una imagen falle todo
-      });
-
+      img.src = this.framePath(i);
       this.preloadedImages.push(img);
-      promises.push(promise);
     }
 
-    await Promise.all(promises);
   }
 
+
+  // ⏯️ Reproducir desde cierto segundo
   iniciarDesde(): void {
     const video = this.zorroVideoSaludo.nativeElement;
     video.currentTime = this.segundos;
     this.segundos = 0;
   }
 
+  // 👁️ Detecta visibilidad del video de conocimientos
   private setupVideoObserver(): void {
     const video = document.getElementById('zorroVideoConocimientos') as HTMLVideoElement;
     const caja = document.getElementById('contenedorVideoConocimientos')!;
 
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        video.currentTime = 0;
-        video.play();
-
-        if (entry.isIntersecting) {
-          caja.classList.remove('opacity-0');
-          caja.classList.add('animate__bounceInRight');
-          setTimeout(() => caja.classList.remove('animate__bounceInRight'), 1000);
-        } else {
-          caja.classList.add('opacity-0');
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
           video.currentTime = 0;
-        }
-      });
-    }, { threshold: 0.1 });
+          video.play();
+
+          if (entry.isIntersecting) {
+            caja.classList.remove('opacity-0');
+            caja.classList.add('animate__bounceInRight');
+            setTimeout(() => caja.classList.remove('animate__bounceInRight'), 1000);
+          } else {
+            caja.classList.add('opacity-0');
+            video.currentTime = 0;
+          }
+        });
+      },
+      { threshold: 0.1 }
+    );
 
     observer.observe(video);
   }
 
+
+
+  // ⏸️ Detiene los videos en cierto punto
   checkTime(zorro: string): void {
     if (zorro === 'saludo' && this.zorroVideoSaludo?.nativeElement.currentTime >= 6) {
       this.zorroVideoSaludo.nativeElement.pause();
@@ -123,6 +170,7 @@ export class HomeComponent implements AfterViewInit {
     }
   }
 
+  // 🔁 Reinicia videos
   reproducir(video: string): void {
     if (video === 'saludo') {
       this.zorroVideoSaludoSaludo = false;
@@ -138,6 +186,31 @@ export class HomeComponent implements AfterViewInit {
     return this.modoOscuro
       ? './assets/misConocimientosModoOscuro.mp4'
       : './assets/conocimientos.mp4';
+  }
+
+
+
+
+  @ViewChild('scrollImage', { static: true }) scrollImageRef!: ElementRef<HTMLImageElement>;
+  @ViewChild('scrollContainer', { static: true }) containerRef!: ElementRef<HTMLDivElement>;
+
+  totalFrames = 232;
+
+  // Construye la ruta del frame
+  framePath(index: number): string {
+    const padded = String(index).padStart(3, '0');
+
+    if (this.modoOscuro == true) {
+      this.totalFrames = 240
+
+
+      return `assets/scroll-video-oscuro/frame_${padded}.jpg`;
+    } else {
+      this.totalFrames = 232
+
+      return `assets/scroll-video/frame_${padded}.jpg`;
+    }
+
   }
 
   inicializarVideoScroll(): void {
@@ -165,7 +238,11 @@ export class HomeComponent implements AfterViewInit {
     });
   }
 
-  animacionCard(): void {
+
+  @ViewChildren('card') cards!: QueryList<ElementRef>;
+
+  animacionCard() {
+
     this.cards.forEach((cardRef: ElementRef, index: number) => {
       const el = cardRef.nativeElement;
 
@@ -173,10 +250,16 @@ export class HomeComponent implements AfterViewInit {
       let fromVars: any = { opacity: 0 };
 
       switch (animationStep) {
-        case 0: fromVars.x = -100; break;
+        case 0:
+          fromVars.x = -100; // izquierda
+          break;
         case 1:
-        case 2: fromVars.y = 100; break;
-        case 3: fromVars.x = 100; break;
+        case 2:
+          fromVars.y = 100; // abajo
+          break;
+        case 3:
+          fromVars.x = 100; // derecha
+          break;
       }
 
       gsap.fromTo(
@@ -197,5 +280,10 @@ export class HomeComponent implements AfterViewInit {
         }
       );
     });
+
   }
+
 }
+
+
+
